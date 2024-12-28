e36d8b37d60357f615a53cd3aa487131b9cc8c00
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2024-12-28 11:43:44 +0100

    [FIX] Changed hybrid join emitter batches storage from channel wise to key wise

--- wf/hybrid_join_emitter.hpp
+++ wf/hybrid_join_emitter.hpp
@@ -59,8 +59,8 @@ private:
     size_t size; // if >0 the emitter works in batched more, otherwise in a per-tuple basis
     bool useTreeMode; // true if the emitter is used in tree-based mode
     std::vector<std::pair<void *, size_t>> output_queue; // vector of pairs (messages and destination identifiers)
-    std::vector<Batch_CPU_t<tuple_t> *> batches_output; // vector of pointers to the output batches one per range of destinations (meaningful if size > 0)
-    std::vector<uint64_t> last_sent_wms; // vector of the last sent watermarks, one per destination
+    std::unordered_map<key_t, Batch_CPU_t<tuple_t> *> batches_output; // map of the output batches one per destination channel (meaningful if size > 0)
+    std::vector<uint64_t> last_sent_wms; // vector of the last sent watermarks, one per destination channel
 
 public:
     // Constructor
@@ -73,7 +73,6 @@ public:
                       hybrid_degree(_hybrid_degree),
                       size(_size),
                       useTreeMode(false),
-                      batches_output(_num_dests, nullptr),
                       last_sent_wms(_num_dests, 0)  {}
 
     // Copy Constructor
@@ -84,16 +83,16 @@ public:
                       hybrid_degree(_other.hybrid_degree),
                       size(_other.size),
                       useTreeMode(_other.useTreeMode),
-                      batches_output(_other.num_dests, nullptr),
                       last_sent_wms(_other.last_sent_wms) {}
 
     // Destructor
     ~HybridJoin_Emitter() override
     {
         assert(output_queue.size() == 0); // sanity check
-        for (auto *b: batches_output) {
+        assert(batches_output.empty()); // sanity check
+        /* for (const auto& [key, b] : batches_output) {
             assert(b == nullptr); // sanity check
-        }
+        } */
         if (size == 0) { // delete all the Single_t items in the recycling queue
             Single_t<tuple_t> *msg = nullptr;
             while ((this->queue)->pop((void **) &msg)) {
@@ -205,11 +204,11 @@ public:
         
         auto key = key_extr(_output->tuple); // extract the key attribute
         size_t hashcode = std::hash<key_t>()(key); // compute the hashcode of the key
-        size_t i = hashcode % num_dests; // compute the initial destination identifier
+        size_t i = hashcode % num_dests; // compute the initial destination identifier (master_id)
         size_t sends = hybrid_degree;
         while(sends > 0) {
-            assert(last_sent_wms[i] <= _output->getWatermark()); // sanity check
-            last_sent_wms[i] = _output->getWatermark(); // save the last watermark emitted to this destination
+            assert(last_sent_wms[i] <= _output->getWatermark(i)); // sanity check
+            last_sent_wms[i] = _output->getWatermark(i); // save the last watermark emitted to this destination
             if (!useTreeMode) { // real send
                 _node->ff_send_out_to(_output, i);
             }
@@ -227,33 +226,38 @@ public:
                          uint64_t _watermark,
                          ff::ff_monode *_node)
     {
-
         auto key = key_extr(_tuple); // extract the key attribute
-        size_t hashcode = std::hash<key_t>()(key); // compute the hashcode of the key
-        size_t initial_id = hashcode % num_dests; // compute the initial destination identifier
-        if (batches_output[initial_id] == nullptr) {
-            batches_output[initial_id] = allocateBatch_CPU_t<tuple_t>(size, this->queue);
+        auto it = batches_output.find(key); // find the corresponding key_descriptor (or allocate it if does not exist)
+        if (it == batches_output.end()) {
+            auto p = batches_output.insert(std::make_pair(key, allocateBatch_CPU_t<tuple_t>(size, this->queue)));
+            it = p.first;
         }
-        auto batch_output = batches_output[initial_id];
-        batch_output->addTuple(std::move(_tuple), _timestamp, _watermark);
-        if (batch_output->getSize() == size) { // batch is ready to be sent
-            (batch_output->delete_counter).fetch_add(hybrid_degree-1);
-            assert((batch_output->watermarks).size() == 1); // sanity check
-            (batch_output->watermarks).insert((batch_output->watermarks).end(), num_dests-1, (batch_output->watermarks)[0]); // copy the watermark (having one per destination)
-
-            size_t i = initial_id; // compute the initial destination identifier
+        Batch_CPU_t<tuple_t> *batch = (*it).second;
+        batch->addTuple(std::move(_tuple), _timestamp, _watermark);
+        
+        if (batch->getSize() == size) { // batch is ready to be sent
+            assert((batch->watermarks).size() == 1); // sanity check
+            // copy the watermark (having one per destination)
+            (batch->watermarks).insert((batch->watermarks).end(), num_dests-1, (batch->watermarks)[0]);
+            (batch->delete_counter).fetch_add(hybrid_degree-1);
+            
+            size_t hashcode = std::hash<key_t>()(key); // compute the hashcode of the key
+            size_t master_id = hashcode % num_dests; // compute the initial destination identifier (master_id)
+            size_t i = master_id;
             size_t sends = hybrid_degree;
             while(sends > 0) {
+                assert(last_sent_wms[i] <= (batch->watermarks)[i]); // sanity check
+                last_sent_wms[i] = (batch->watermarks)[i]; // save the last watermark emitted to this destination
                 if (!useTreeMode) { // real send
-                    _node->ff_send_out_to(batch_output, i);
+                    _node->ff_send_out_to(batch, i);
                 }
                 else { // batch_output is buffered
-                    output_queue.push_back(std::make_pair(batch_output, i));
+                    output_queue.push_back(std::make_pair(batch, i));
                 }
                 i = (i+1) % num_dests;
                 sends--;
             }
-            batches_output[initial_id] = nullptr;
+            batches_output.erase(it); // delete the batch from the map
         }
     }
 
@@ -300,26 +304,55 @@ public:
     void flush(ff::ff_monode *_node) override
     {
         if (size > 0) { // only batching
-            for (size_t i=0; i<num_dests; i++) {
+            for (auto it = batches_output.begin(); it != batches_output.end(); ++it) {
+                key_t key = it->first;
+                Batch_CPU_t<tuple_t> *batch = it->second;
+                size_t hashcode = std::hash<key_t>()(key); // compute the hashcode of the key
+                size_t master_id = hashcode % num_dests; // compute the initial destination identifier (master_id)
+
+                assert((batch->watermarks).size() == 1); // sanity check
+                // copy the watermark (having one per destination)
+                (batch->watermarks).insert((batch->watermarks).end(), num_dests-1, (batch->watermarks)[0]);
+                (batch->delete_counter).fetch_add(hybrid_degree-1);
+
+                size_t i = master_id;
+                size_t sends = hybrid_degree;
+                while(sends > 0) {
+                    assert(last_sent_wms[i] <= (batch->watermarks)[i]); // sanity check
+                    last_sent_wms[i] = (batch->watermarks)[i]; // save the last watermark emitted to this destination
+                    if (!useTreeMode) { // real send
+                        _node->ff_send_out_to(batch, i);
+                    }
+                    else { // batch_output is buffered
+                        output_queue.push_back(std::make_pair(batch, i));
+                    }
+                    i = (i+1) % num_dests;
+                    sends--;
+                }
+                //batches_output.erase(it); // delete the batch from the map
+                //std::cout << "Erased" << std::endl;
+            }
+            batches_output.clear();           
+            /* for (size_t i=0; i<num_dests; i++) {
                 if (batches_output[i] != nullptr) {
                     assert(batches_output[i]->getSize() > 0); // sanity check
                     (batches_output[i]->delete_counter).fetch_add(hybrid_degree-1);
                     (batches_output[i]->watermarks).insert((batches_output[i]->watermarks).end(), num_dests-1, (batches_output[i]->watermarks)[0]); // copy the watermark (having one per destination)
-                    size_t j = i; // compute the initial destination identifier
+                    size_t j = i; // master_id
                     size_t sends = hybrid_degree;
                     while(sends > 0) {
                         if (!useTreeMode) { // real send
-                            _node->ff_send_out_to(batches_output[i], i);
+                            _node->ff_send_out_to(batches_output[i], j);
                         }
                         else { // output is buffered
-                            output_queue.push_back(std::make_pair(batches_output[i], i));
+                            output_queue.push_back(std::make_pair(batches_output[i], j));
                         }
                         j = (j+1) % num_dests;
                         sends--;
                     }
                     batches_output[i] = nullptr;
                 }
-            }
+            } */
         }
     }
 
